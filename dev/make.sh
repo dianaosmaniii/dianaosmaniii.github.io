3d05be9ba85cff15ac01564182d847cb4fee04db
Diana Osmani <dianaosmani.fna@gmail.com>
2019-07-27 17:56:49 +0200

    Update

--- dev/make.sh
+++ dev/make.sh
@@ -1,3 +1,4 @@
+html-partials-compiler index.html > ../index.html
 html-partials-compiler web/u2/1_simplemap.html > ../web/u2/1_simplemap.html
 html-partials-compiler web/u2/2_syncsimplemap.html > ../web/u2/2_syncsimplemap.html
 html-partials-compiler web/u2/3_controls.html > ../web/u2/3_controls.html
