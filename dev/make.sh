f20abdbeaad33ea0578da60c185ae90277b46b5a
Diana Osmani <dianaosmani.fna@gmail.com>
2019-07-31 02:50:37 +0200

    Update

--- dev/make.sh
+++ dev/make.sh
@@ -37,3 +37,4 @@ html-partials-compiler web/u4/18_Proj4JsTransformim.html > ../web/u4/18_Proj4JsT
 html-partials-compiler web/u4/19_StringFormat.html > ../web/u4/19_StringFormat.html
 html-partials-compiler architecture.html > ../architecture.html
 html-partials-compiler doc.html > ../doc.html
+html-partials-compiler gis.html > ../gis.html
